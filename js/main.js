14436e220a923f8bc679c96635236c48b5d1298a
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2021-09-03 10:02:53 +0200

    create video section

--- js/main.js
+++ js/main.js
@@ -42,3 +42,33 @@ var swiper = new Swiper(".discover__container", {
     rotate: 0,
   },
 });
+
+const videoFile = document.getElementById("video-file");
+const videoButton = document.getElementById("video-button");
+const videoIcon = document.getElementById("video-icon");
+
+function playPause() {
+  if (videoFile.paused) {
+    // Play video
+    videoFile.play();
+
+    // change icon
+    videoIcon.classList.add("ri-pause-line");
+    videoIcon.classList.remove("ri-play-line");
+  } else {
+    //  Pause video
+    videoFile.pause();
+    // change icon
+    videoIcon.classList.remove("ri-pause-line");
+    videoIcon.classList.add("ri-play-line");
+  }
+}
+
+videoButton.addEventListener("click", playPause);
+
+function finishVideo() {
+  videoIcon.classList.remove("ri-pause-line");
+  videoIcon.classList.add("ri-play-line");
+}
+
+videoFile.addEventListener("ended", finishVideo);
