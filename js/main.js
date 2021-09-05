ea15708e30056e146c0067bc29f0d79e478e1c7e
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2021-09-05 18:55:47 +0200

    add media queries

--- js/main.js
+++ js/main.js
@@ -72,3 +72,11 @@ function finishVideo() {
 }
 
 videoFile.addEventListener("ended", finishVideo);
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
